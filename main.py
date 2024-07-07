2693b39e964eb1d4e53c11776ed957a57d33819a
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2024-07-07 05:10:04 +0000

    Problem 2: Solved

--- main.py
+++ main.py
@@ -20,3 +20,16 @@ bulbasaur = Pokemon("Bulbasaur", 45, 30)
 
 opponent = bulbasaur
 pikachu.attack(opponent)
+
+# Problem 2: Convert to Linked List
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+node_1 = Node("Jigglypuff")
+node_2 = Node("Wigglytuff")
+node_1.next = node_2
+print(node_1.value, "->", node_1.next.value)
+print(node_2.value, "->", node_2.next)
